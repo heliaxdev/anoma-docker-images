16a29630b251b51fdad7089008304424736d42fa
Samuli Thomasson <samuli.thomasson@paivola.fi>
2021-11-19 12:21:52 +0100

    upgrade nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 let
-  hash = "ee084c02040e864eeeb4cf4f8538d92f7c675671"; # nixpkgs-unstable 04-10-2021
+  hash = "7fad01d9d5a3f82081c00fb57918d64145dc904c"; # nixpkgs-unstable 2021-11-17
 in
   builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/${hash}.tar.gz"
